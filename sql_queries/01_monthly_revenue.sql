bc0d0280a86e95ffdbe17a97bd084036a37d5ec4
sergio-garcia940 <sergio-garcia@live.cl>
2026-02-28 12:24:09 -0300

    Create 01_monthly_revenue.sql

---
+++ sql_queries/01_monthly_revenue.sql
@@ -0,0 +1,8 @@
+SELECT
+  FORMAT_DATE('%Y-%m', DATE(o.order_purchase_timestamp)) AS year_month,
+  ROUND(SUM(p.payment_value),2) AS monthly_revenue
+FROM `ecommerce_analysis.olist_orders_dataset` o
+JOIN `ecommerce_analysis.olist_order_payments_dataset` p
+ON o.order_id = p.order_id
+GROUP BY year_month
+ORDER BY year_month;
